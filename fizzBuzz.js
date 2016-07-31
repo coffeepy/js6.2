22b4e58cf129d0c0ffd439a4696fc43a5b295fe7
coffeepy <goddardbr@gmail.com>
2016-07-31 13:05:46 -0500

    Made Corrections

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,16 @@
 function fizzBuzz() {
   for (var iCtr = 1; iCtr<=100; iCtr++){
-    if (iCtr % 3 == 0){
-      console.log(iCtr + ' Fizz');
-    };
-    if (iCtr % 5 == 0) {
-      console.log(iCtr + ' Buzz');
-    };
     if (iCtr % 3 == 0 && iCtr % 5 == 0) {
       console.log(iCtr + ' FizzBuzz');
+    }
+    else if (iCtr % 3 == 0){
+      console.log(iCtr + ' Fizz');
+    }
+    else if (iCtr % 5 == 0) {
+      console.log(iCtr + ' Buzz');
+    }
+    else {
+      console.log(iCtr);
     };
   };
 };
